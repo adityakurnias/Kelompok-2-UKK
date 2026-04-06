fb2f5d633bbeb8a27657891b502ffc65338babd9
Kurnias <adityakurniasaputra903@gmail.com>
2026-04-06 18:21:31 +0700

    Uncomment role enum definition in users table migration

--- Fariz/database/migrations/0001_01_01_000000_create_users_table.php
+++ Fariz/database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            // $table->enum('role', ['admin', 'user'])->default('user');
+            $table->enum('role', ['admin', 'user'])->default('user');
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
